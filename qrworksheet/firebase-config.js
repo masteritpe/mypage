421d34c2e25ac8ba0d2188097f4ea8b92154826b
masteritpe <janghs.itpe@gmail.com>
2026-07-07 12:39:40 +0900

    데이터 업로드
    데이터 업로드

---
+++ qrworksheet/firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+window.firebaseConfig = {
+    apiKey: "AIzaSyA6cwFwIO83E7vCIL8vGcmY9EOIDfYN2qg",
+    authDomain: "school-workbook.firebaseapp.com",
+    projectId: "school-workbook",
+    storageBucket: "school-workbook.firebasestorage.app",
+    messagingSenderId: "1085308072419",
+    appId: "1:1085308072419:web:cfbbdf9357c9d567ddf0d6"
+};
